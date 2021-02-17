6d6f3ef69abcd3fd88681c4aa17a3be971c308dd
Praz <t.prazwal@gmail.com>
2021-02-17 17:57:45 +0100

    create rootReducer, store and add in src/index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+import INITIAL_STATE from '../constants/initialState';
+import loginReducer from '../reducers/loginReducer';
+
+const rootReducer = combineReducers({
+  login: loginReducer,
+});
+
+const store = createStore(rootReducer, INITIAL_STATE);
+
+export default store;
